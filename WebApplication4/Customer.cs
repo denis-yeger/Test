17b4f02690716e35d55c31107335f0c18aeba0bb
Denys Yeger <denis.yeger@gmail.com>
2017-04-17 23:32:41 +0300

    Add project files.

---
+++ WebApplication4/Customer.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.Serialization;
+namespace WebApplication4
+{
+    [DataContract]
+    public class Customer
+    {
+        [DataMember]
+        public string Name { get; set; }
+    }
+}
---
+++ WebApplication4/Startup1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+using System.Net.Http.Formatting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+[assembly: OwinStartup(typeof(WebApplication4.Startup1))]
+
+namespace WebApplication4
+{
+    public class Startup1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            config.Routes.MapHttpRoute("Default", "V1/{controller}/{id}");
+
+            config.Formatters.Clear();
+            config.Formatters.Add(new JsonMediaTypeFormatter());
+            config.Formatters.JsonFormatter.SerializerSettings =
+            new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            };
+
+            app.UseWebApi(config);
+        }
+    }
+}
